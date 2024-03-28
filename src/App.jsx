c3520af35eae2d997c71c803572d8f93c5601eef
TomoriComAncora <lucas_fernando@ufms.br>
2024-03-28 12:45:38 -0400

    pegando idades

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState, useEffect } from 'react';
-import api from './services/api';
-import './App.css'
+import { useState, useEffect } from "react";
+import api from "./services/api";
+import "./App.css";
 
 function App() {
   const [jogadores, setJogadores] = useState([]);
 
-  useEffect(()=>{
-    async function loadJogadores(){
-      const response = await api.get('rating/fc-24?limit=100')
+  useEffect(() => {
+    async function loadJogadores() {
+      const response = await api.get("rating/fc-24");
 
-      console.log(response.data.items.slice(0,10));
-      setJogadores(response.data.items.slice(0,20));
+      const jogadoresData = response.data.items.slice(0, 50);
+      
+      // setJogadores(response.data.items.slice(0, 20));
+      const jogadoresComIdade = jogadoresData.map((jogador) => {
+        const birthdateDate = new Date(jogador.birthdate);
+        const today = new Date();
+        const diffYears = today.getFullYear() - birthdateDate.getFullYear();
+        const isBeforeBirthday = today.getMonth() < birthdateDate.getMonth() ||
+          (today.getMonth() === birthdateDate.getMonth() && today.getDate() < birthdateDate.getDate());
+        const idade = isBeforeBirthday ? diffYears - 1 : diffYears;
+        return { ...jogador, idade };
+      });
+      console.log(jogadoresComIdade);
+      setJogadores(jogadoresComIdade);
+    
     }
 
     loadJogadores();
-  }, [])
+  }, []);
   // items.lastName
   return (
     <div>
-      {jogadores.map((items)=>(
-        <article className='card' key={items.id}>
-          <strong>{items.commonName !== null ? items.commonName:items.lastName}</strong>
+      {jogadores.map((items, index) => (
+        <article className="card" key={items.id}>
+          <strong>
+            {items.commonName !== null ? items.commonName : items.lastName}
+          </strong>
           <img src={items.avatarUrl} alt={items.lastName} />
           <p>{items.nationality.label}</p>
           <p>{items.overallRating}</p>
-          <p>{items.position.label}/{items.position.shortLabel}</p>
-          <p>{items.skillMoves}/{items.weakFootAbility}</p>
+          <p>
+            {items.position.label}/{items.position.shortLabel}
+          </p>
+          <p>
+            {items.skillMoves}/{items.weakFootAbility}
+          </p>
+          <p>Idade: {items.idade}</p>
         </article>
       ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
